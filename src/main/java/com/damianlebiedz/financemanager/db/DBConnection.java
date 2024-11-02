217524244b804ff7555b8670f0aa430d46363108
Damian Lebiedź <109239676+damianlebiedz@users.noreply.github.com>
2024-11-02 13:24:13 +0100

    Update DBConnection.java
    description

--- src/main/java/com/damianlebiedz/financemanager/db/DBConnection.java
+++ src/main/java/com/damianlebiedz/financemanager/db/DBConnection.java
@@ -5,11 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    String url = "jdbc:h2:~/data";
+    String url = "jdbc:h2:~/data"; //Adres URL bazy danych H2 (w folderze data)
     String username = "sa";
     String password = "123";
 
-    public Connection getConnection() {
+    public Connection getConnection() { //Metoda do uzyskiwania połączenia z bazą danych
         try {
             return DriverManager.getConnection(url, username, password);
         }
