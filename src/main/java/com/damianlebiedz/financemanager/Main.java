e48188ef8ff677272b2ab97d3163c1e2f4b83a08
Damian Lebiedź <109239676+damianlebiedz@users.noreply.github.com>
2024-11-02 13:22:19 +0100

    Update Main.java
    descriptions

--- src/main/java/com/damianlebiedz/financemanager/Main.java
+++ src/main/java/com/damianlebiedz/financemanager/Main.java
@@ -12,17 +12,15 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            //Dodawanie FXML i ustalenie wielkości okna aplikacji
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/MainView.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/MainView.fxml")); //Dodawanie FXML i ustalenie wielkości okna aplikacji
+
             int width = 1080;
             int height = 720;
             Scene scene = new Scene(fxmlLoader.load(), width, height);
+            
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/style.css")).toExternalForm());  //Dodawanie CSS
 
-            //Dodawanie CSS
-            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/style.css")).toExternalForm());
-
-            //Dodawanie ikony i tytułu aplikacji
-            Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/icon.png")));
+            Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/icon.png"))); //Dodawanie ikony i tytułu aplikacji
             stage.getIcons().add(icon);
             stage.setTitle("Finance Manager");
             stage.setResizable(false);
@@ -34,8 +32,7 @@ public class Main extends Application {
         }
     }
     
-    // Główna metoda aplikacji
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Główna metoda aplikacji
         launch();
     }
 }
